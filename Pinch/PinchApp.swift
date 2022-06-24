218402d236ed4d5135669c13ef1add1547074e3d
Alessia Masola <alessiamasola23@gmail.com>
2022-06-24 16:35:19 +0900

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Alessia Masola on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
