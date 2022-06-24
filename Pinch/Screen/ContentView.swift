836b0c0a8936223e4bdd2de725cf5b2d27d0c222
Alessia Masola <alessiamasola23@gmail.com>
2022-06-24 19:46:03 +0900

    Added: - Drag gesture, handling both the zoomed in and the zoomed out state

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -10,9 +10,16 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - PROPERTY
     @State private var isAnimating: Bool = false
-    @State private var imageScale: CGFloat = 1
+    @State private var imageScale: CGFloat = 1.0
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - FUNCTION
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - CONTENT
     var body: some View {
@@ -26,6 +33,8 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                // This has to be added before the scaleEffect. Order matters!
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .animation(.linear(duration: 1), value: isAnimating)
                     .scaleEffect(imageScale)
                 // MARK: - 1. TAP GESTURE
@@ -35,11 +44,23 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                // MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)){
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
                 
                 
             } //: ZSTACK
