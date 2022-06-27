193546315abd52d8590614687a036afa9f58ae66
Alessia Masola <alessiamasola23@gmail.com>
2022-06-27 22:56:46 +0900

    Added a Control Interface with 'plus', 'minus', and 'reset' buttons.

---
+++ Pinch/View/ControlImageView.swift
@@ -0,0 +1,25 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Alessia Masola on 27/06/2022.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
